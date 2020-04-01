4653d2af83e188a52c4a139c08508712c16141e9
Sean Downs <spdowns@protonmail.com>
2020-04-01 13:37:52 -0700

    the letter a works right now

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,27 @@
 var vowels = ['a', 'e', 'i', 'o', 'u']
 var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
 
+
+
 var finish = function(word) {
   var firstLetter = word.charAt(0); 
+  console.log(firstLetter)
   // loop through vowels array and see if your char matches
   // loop through consonant array and see if your char matches
-  if (firstLetter === vowels) {
+  var n = vowels.indexOf(firstLetter);
+  console.log(n)
+  if (n === 0) {
     return (word + " way");
   } else { 
-    return (word + " backwards")
+    return (word + " not working")
   }
-  return word + "way";
 };
+console.log(vowels)
 
-for(var i = 0; i < vowels.length; i++){
-  console.log(vowels[i]);
-  // for(var j = 0; j < consonants.length; i++){
-  //   console.log(consonants[j]);
+// for(var i = 0; i < vowels.length; i++){
+//   console.log(vowels[i]);
+  for(var j = 0; j < consonants.length; j++){
+    console.log(consonants[j]);
   //   console.log(vowels[i]);
   //   console.log(consonants[j] + vowels[i])
   // }
