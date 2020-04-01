fb0590c58e9b01397df48627ab762ced6552ccf8
Sean Downs <spdowns@protonmail.com>
2020-04-01 15:42:20 -0700

    tests array split with .split function

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
 
-var vowels = ['a', 'e', 'i', 'o', 'u']
-var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+var vowels = ['a', 'e', 'i', 'o', 'u', 'A','E','I','O', 'U']
+var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z','B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+var sentence = 
 
 
 var finish = function(word) {
+  
   var firstLetter = word.charAt(0);
-  var secondLetter = word.charAt(1) 
-  var result = []
+  var secondLetter = word.charAt(1);
+  var result = [];
+
+
   if (vowels.includes(firstLetter)) {
   // loop through vowels array and see if your char matches
   // loop through consonant array and see if your char matches
     return (word + "way");
-  }   else if (consonants.includes(firstLetter)&& consonants.includes(secondLetter)) {
-      result.push(word.slice(2, word.length) + word[0] + word [1] + 'ay');
-      return result.join('');
+  } else if (consonants.includes(firstLetter) && consonants.includes(secondLetter)) {
+    result.push(word.slice(2, word.length) + word[0] + word [1] + 'ay');
+    return result.join('');
   } else if (consonants.includes(firstLetter)) {
     result.push(word.slice(1, word.length) + word [0] + 'ay');
     return result.join('');
-  
   }
+  
 };
 
 // for(var i = 0; i < vowels.length; i++){
 //   console.log(vowels[i]);
-  for(var j = 0; j < consonants.length; j++){
-    console.log(consonants[j]);
+
   //   console.log(vowels[i]);
   //   console.log(consonants[j] + vowels[i])
   // }
-}
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
 
@@ -38,7 +40,6 @@ $(document).ready(function(){
 
     var word = $("input#input1").val();
     var result = finish(word);
- 
   
     $("#result").html(result);   
   });
